8e05ca3508aaa7f02e29e0dfdf7c02165a6d7024
Bilal ATLI <bilal@sistemkoin.com>
2019-07-15 21:09:30 +0300

    Add Insert Sort

--- src/Arrays/Algorithms/InsertSort.php
+++ src/Arrays/Algorithms/InsertSort.php
@@ -3,6 +3,9 @@
 namespace BALib\Sorting\Algorithms;
 
 
+use BALib\Abstracts\ASorting;
+use BALib\Sorting\Interfaces\Sorting;
+
 /**
  * @author  : Bilal ATLI
  * @date    : 30.03.2019 15:44
@@ -11,7 +14,32 @@ namespace BALib\Sorting\Algorithms;
  *
  * @package BALib\Sorting\Algorithms;
  */
-class InsertSort
+class InsertSort extends ASorting implements Sorting
 {
 
+    /**
+     * Sort Array
+     *
+     * @param array &$array
+     *
+     * @return array
+     */
+    public static function sort(array &$array): array
+    {
+        self::timerStart();
+        $count = count($array);
+        for ($i = 0; $i < ( $count - 1 ); $i++) {
+            $key = $i + 1;
+            $tmp = $array[$key];
+            for ($j = ( $i + 1 ); $j > 0; $j--) {
+                if ($tmp < $array[$j - 1]) {
+                    $array[$j] = $array[$j - 1];
+                    $key = $j - 1;
+                }
+            }
+            $array[$key] = $tmp;
+        }
+        self::timerStop();
+        return $array;
+    }
 }
