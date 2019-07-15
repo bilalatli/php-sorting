08d899b29fd0f43e3c6db211401cbd25d9da8042
Bilal ATLI <bilal@sistemkoin.com>
2019-07-15 21:09:59 +0300

    Add Selection Sort

--- src/Arrays/Algorithms/SelectionSort.php
+++ src/Arrays/Algorithms/SelectionSort.php
@@ -3,6 +3,9 @@
 namespace BALib\Sorting\Algorithms;
 
 
+use BALib\Abstracts\ASorting;
+use BALib\Sorting\Interfaces\Sorting;
+
 /**
  * @author  : Bilal ATLI
  * @date    : 30.03.2019 15:44
@@ -11,7 +14,32 @@ namespace BALib\Sorting\Algorithms;
  *
  * @package BALib\Sorting\Algorithms;
  */
-class SelectionSort
+class SelectionSort extends ASorting implements Sorting
 {
 
+    /**
+     * Sort Array
+     *
+     * @param array &$array
+     *
+     * @return array
+     */
+    public static function sort(array &$array): array
+    {
+        self::timerStart();
+        $count = count($array);
+        for ($i = 0; $i < ( $count - 1 ); $i++) {
+            $key = $i;
+            for ($j = ( $i + 1 ); $j < $count; $j++) {
+                if ($array[$j] < $array[$key]) {
+                    $key = $j;
+                }
+            }
+            if ($key != $i) {
+                list($array[$key], $array[$i]) = [ $array[$i], $array[$key] ];
+            }
+        }
+        self::timerStop();
+        return $array;
+    }
 }
