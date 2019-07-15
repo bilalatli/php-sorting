824c09a5c2fecd3a5c16a3e6648cdda820d7bcac
Bilal ATLI <bilal@sistemkoin.com>
2019-07-15 21:08:33 +0300

    Add Cocktail Sort

--- src/Arrays/Algorithms/CocktailSort.php
+++ src/Arrays/Algorithms/CocktailSort.php
@@ -3,6 +3,9 @@
 namespace BALib\Sorting\Algorithms;
 
 
+use BALib\Abstracts\ASorting;
+use BALib\Sorting\Interfaces\Sorting;
+
 /**
  * @author  : Bilal ATLI
  * @date    : 30.03.2019 15:44
@@ -11,7 +14,37 @@ namespace BALib\Sorting\Algorithms;
  *
  * @package BALib\Sorting\Algorithms;
  */
-class CocktailSort
+class CocktailSort extends ASorting implements Sorting
 {
 
+    /**
+     * Sort Array
+     *
+     * @param array &$array
+     *
+     * @return array
+     */
+    public static function sort(array &$array): array
+    {
+        self::timerStart();
+        $count = count($array);
+        $left = 0;
+        $right = $count - 1;
+        do {
+            for ($i = $left; $i < $right; $i++) {
+                if ($array[$i] > $array[$i + 1]) {
+                    list($array[$i], $array[$i + 1]) = [ $array[$i + 1], $array[$i] ];
+                }
+            }
+            $right -= 1;
+            for ($i = $right; $i > $left; $i--) {
+                if ($array[$i] < $array[$i - 1]) {
+                    list($array[$i], $array[$i - 1]) = [ $array[$i - 1], $array[$i] ];
+                }
+            }
+            $left += 1;
+        } while ($left <= $right);
+        self::timerStop();
+        return $array;
+    }
 }
