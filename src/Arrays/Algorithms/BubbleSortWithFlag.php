24fc289f5b04b30bcc86a1a0628f8edfc5d0bf2d
Bilal ATLI <bilal@sistemkoin.com>
2019-07-15 21:08:21 +0300

    Add Bubble Sort With Flag Algorithm

---
+++ src/Arrays/Algorithms/BubbleSortWithFlag.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace BALib\Sorting\Algorithms;
+
+
+use BALib\Abstracts\ASorting;
+use BALib\Sorting\Interfaces\Sorting;
+
+/**
+ * @author  : Bilal ATLI
+ * @date    : 30.03.2019 15:44
+ * @mail    : <bilal@garivaldi.com>, <ytbilalatli@gmail.com>
+ * @phone   : +90 0-542-433-09-19
+ *
+ * @package BALib\Sorting\Algorithms;
+ */
+class BubbleSortWithFlag extends ASorting implements Sorting
+{
+
+    /**
+     * Sort Array
+     *
+     * @param array &$array
+     *
+     * @return array
+     */
+    public static function sort(array &$array): array
+    {
+        self::timerStart();
+        $count = count($array);
+        for ($j = 0; $j < ( $count - 1 ); $j++) {
+            $flag = false;
+            for ($i = 0; $i < ( $count - $j - 1 ); $i++) {
+                if ($array[$i] > $array[$i + 1]) {
+                    list($array[$i], $array[$i + 1]) = [ $array[$i + 1], $array[$i] ];
+                    if (!$flag) {
+                        $flag = true;
+                    }
+                }
+            }
+            if (!$flag) {
+                break;
+            }
+        }
+        self::timerStop();
+        return $array;
+    }
+}
---
+++ src/Arrays/Algorithms/SelectionSort.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BALib\Sorting\Algorithms;
+
+
+/**
+ * @author  : Bilal ATLI
+ * @date    : 30.03.2019 15:44
+ * @mail    : <bilal@garivaldi.com>, <ytbilalatli@gmail.com>
+ * @phone   : +90 0-542-433-09-19
+ *
+ * @package BALib\Sorting\Algorithms;
+ */
+class SelectionSort
+{
+
+}
