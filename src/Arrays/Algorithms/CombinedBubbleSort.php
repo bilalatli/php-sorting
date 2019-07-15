da688aede4fd898d8686f7f0272c356494d676e7
Bilal ATLI <bilal@sistemkoin.com>
2019-07-15 21:09:02 +0300

    Add Combined Bubble Sort

--- src/Arrays/Algorithms/CombinedBubbleSort.php
+++ src/Arrays/Algorithms/CombinedBubbleSort.php
@@ -3,6 +3,9 @@
 namespace BALib\Sorting\Algorithms;
 
 
+use BALib\Abstracts\ASorting;
+use BALib\Sorting\Interfaces\Sorting;
+
 /**
  * @author  : Bilal ATLI
  * @date    : 30.03.2019 15:44
@@ -11,7 +14,42 @@ namespace BALib\Sorting\Algorithms;
  *
  * @package BALib\Sorting\Algorithms;
  */
-class CombinedBubbleSort
+class CombinedBubbleSort extends ASorting implements Sorting
 {
 
+    /**
+     * Sort Array
+     *
+     * @param array &$array
+     *
+     * @return array
+     */
+    public static function sort(array &$array): array
+    {
+        self::timerStart();
+        $count = count($array);
+        for ($j = 0; $j < ( $count - 1 ); $j++) {
+            $flag = false;
+            $min = $j;
+            for ($i = $j; $i < ( $count - $j - 1 ); $i++) {
+                if ($array[$i] > $array[$i + 1]) {
+                    list($array[$i], $array[$i + 1]) = [ $array[$i + 1], $array[$i] ];
+                    if (!$flag) {
+                        $flag = true;
+                    }
+                }
+                if ($array[$i] < $array[$min]) {
+                    $min = $i;
+                }
+            }
+            if (!$flag) {
+                break;
+            }
+            if ($min != $j) {
+                list($array[$min], $array[$j]) = [ $array[$j], $array[$min] ];
+            }
+        }
+        self::timerStop();
+        return $array;
+    }
 }
